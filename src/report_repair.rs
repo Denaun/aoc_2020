1e263721b8546cc06a6b94dc1ac22baaaf16c318
Denaun <mauzuc90@yahoo.it>
2020-12-01 19:55:18 +0100

    Implement day 1 (part 2).

--- src/report_repair.rs
+++ src/report_repair.rs
@@ -1,33 +1,58 @@
 /// Day 1
-use num_traits::Num;
-use std::str::FromStr;
+use num_traits::{CheckedSub, Zero};
+use std::{ops::Mul, str::FromStr};
 
-pub fn find_sum<T: Num + Copy>(values: &[T], sum: T) -> Option<(T, T)> {
-    values[..values.len() - 1]
-        .iter()
-        .enumerate()
-        .find_map(|(ix, &fst)| {
-            let target = sum - fst;
-            if let Some(snd) = values[ix + 1..].iter().copied().find(|&snd| snd == target) {
-                Some((fst, snd))
+fn find_sum<T: Zero + CheckedSub + Copy>(values: &[T], sum: T, n: usize) -> Option<Vec<T>> {
+    if n == 0 {
+        return if sum.is_zero() {
+            Some(Vec::new())
+        } else {
+            None
+        };
+    }
+
+    values.iter().enumerate().skip(n - 1).find_map(|(ix, v)| {
+        if let Some(sum) = sum.checked_sub(v) {
+            if let Some(mut values) = find_sum(&values[..ix], sum, n - 1) {
+                values.push(*v);
+                Some(values)
             } else {
                 None
             }
-        })
+        } else {
+            None
+        }
+    })
 }
 
-pub fn parse_input<T: FromStr>(text: &str) -> Result<Vec<T>, <T as FromStr>::Err> {
+fn parse_input<T: FromStr>(text: &str) -> Result<Vec<T>, <T as FromStr>::Err> {
     text.lines().map(str::parse).collect()
 }
 
 trait Solution {
     fn part_1(&self) -> u32;
+    fn part_2(&self) -> u32;
 }
 impl Solution for str {
     fn part_1(&self) -> u32 {
-        let (fst, snd) = find_sum(&parse_input(self).expect("Failed to parse the input"), 2020)
-            .expect("Solution not found");
-        fst * snd
+        find_sum(
+            &parse_input(self).expect("Failed to parse the input"),
+            2020,
+            2,
+        )
+        .expect("Solution not found")
+        .iter()
+        .fold(1, Mul::mul)
+    }
+    fn part_2(&self) -> u32 {
+        find_sum(
+            &parse_input(self).expect("Failed to parse the input"),
+            2020,
+            3,
+        )
+        .expect("Solution not found")
+        .iter()
+        .fold(1, Mul::mul)
     }
 }
 
@@ -54,13 +79,26 @@ mod tests {
     #[test]
     fn example_1() {
         assert_eq!(
-            find_sum(&[1721, 979, 366, 299, 675, 1456], 2020),
-            Some((1721, 299))
+            find_sum(&[1721, 979, 366, 299, 675, 1456], 2020, 2),
+            Some(vec![1721, 299])
         );
     }
 
     #[test]
     fn part_1() {
         assert_eq!(include_str!("inputs/day_1").part_1(), 776064);
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(
+            find_sum(&[1721, 979, 366, 299, 675, 1456], 2020, 3),
+            Some(vec![979, 366, 675])
+        );
+    }
+
+    #[test]
+    fn part_2() {
+        assert_eq!(include_str!("inputs/day_1").part_2(), 6964490);
     }
 }
