011fedfd5859eff5fa7e51150a32c0108c2c2052
Denaun <mauzuc90@yahoo.it>
2020-12-03 18:40:20 +0100

    Implement day 3 (part 2).

--- src/toboggan_trajectory.rs
+++ src/toboggan_trajectory.rs
@@ -1,5 +1,6 @@
 /// Day 3
 use nom::{alt, character::complete::char, many0, map, named, separated_list0};
+use std::ops::Mul;
 
 fn trajectory<'a>(
     start: (usize, usize),
@@ -18,6 +19,10 @@ fn trajectory<'a>(
         })
 }
 
+fn count_trees(map: &[impl AsRef<[bool]>], slope: (usize, usize)) -> usize {
+    trajectory((0, 0), &map, slope).filter(|v| *v).count()
+}
+
 named!(parse_cell<&str, bool>, alt!(map!(char('.'), |_| false) | map!(char('#'), |_| true)));
 named!(
     parse_input<&str, Vec<Vec<bool>>>,
@@ -26,16 +31,21 @@ named!(
 
 trait Solution {
     fn part_1(&self) -> usize;
+    fn part_2(&self) -> usize;
 }
 impl Solution for str {
     fn part_1(&self) -> usize {
-        trajectory(
-            (0, 0),
+        count_trees(
             &parse_input(self).expect("Failed to parse the input").1,
             (3, 1),
         )
-        .filter(|v| *v)
-        .count()
+    }
+    fn part_2(&self) -> usize {
+        let map = &parse_input(self).expect("Failed to parse the input").1;
+        [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+            .iter()
+            .map(|&slope| count_trees(&map, slope))
+            .fold(1, Mul::mul)
     }
 }
 
@@ -84,5 +94,35 @@ mod tests {
     #[test]
     fn part_1() {
         assert_eq!(include_str!("inputs/day_3").part_1(), 230);
+    }
+
+    #[test]
+    fn example_2() {
+        let map = parse_input(
+            "\
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#",
+        )
+        .unwrap()
+        .1;
+        assert_eq!(count_trees(&map, (1, 1)), 2);
+        assert_eq!(count_trees(&map, (3, 1)), 7);
+        assert_eq!(count_trees(&map, (5, 1)), 3);
+        assert_eq!(count_trees(&map, (7, 1)), 4);
+        assert_eq!(count_trees(&map, (1, 2)), 2);
+    }
+
+    #[test]
+    fn part_2() {
+        assert_eq!(include_str!("inputs/day_3").part_2(), 9_533_698_720);
     }
 }
