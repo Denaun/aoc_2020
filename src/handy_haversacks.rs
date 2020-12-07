b9f051642f88f51f6c15f317ea5cea6d2a88e9fb
Denaun <mauzuc90@yahoo.it>
2020-12-07 19:52:33 +0100

    Implement day 7 (part 1).

---
+++ src/handy_haversacks.rs
@@ -0,0 +1,160 @@
+//! Day 7
+
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{alpha1, char, digit1, line_ending},
+    combinator::{all_consuming, map_res, opt, recognize, value},
+    multi::separated_list1,
+    sequence::{separated_pair, terminated},
+    IResult,
+};
+use std::{
+    collections::{HashMap, HashSet},
+    hash::Hash,
+};
+
+fn build_containee_map<K1, K2>(data: &[(K1, impl AsRef<[(K2, usize)]>)]) -> HashMap<K2, Vec<K1>>
+where
+    K1: Eq + Hash + Clone,
+    K2: Eq + Hash + Clone,
+{
+    let mut result = HashMap::<_, Vec<_>>::new();
+    for (container, containees) in data.iter() {
+        for (containee, _) in containees.as_ref().iter() {
+            result
+                .entry(containee.clone())
+                .or_default()
+                .push(container.clone())
+        }
+    }
+    result
+}
+fn bfs<'a, T>(start: T, data: &'a HashMap<T, Vec<T>>) -> HashSet<&'a T>
+where
+    T: Eq + Hash,
+{
+    let mut all_children = HashSet::new();
+    let mut to_visit = vec![&start];
+    while let Some(next) = to_visit.pop() {
+        if let Some(children) = data.get(next) {
+            for child in children {
+                if !all_children.contains(child) {
+                    to_visit.push(child);
+                    all_children.insert(child);
+                }
+            }
+        }
+    }
+    all_children
+}
+
+fn parse_color(s: &str) -> IResult<&str, &str> {
+    recognize(separated_pair(alpha1, char(' '), alpha1))(s)
+}
+fn parse_quantified_bag(s: &str) -> IResult<&str, (&str, usize)> {
+    let (s, num) = map_res(digit1, |s: &str| s.parse())(s)?;
+    let (s, _) = char(' ')(s)?;
+    let (s, color) = parse_color(s)?;
+    let (s, _) = terminated(tag(" bag"), opt(char('s')))(s)?;
+    Ok((s, (color, num)))
+}
+fn parse_line(s: &str) -> IResult<&str, (&str, Vec<(&str, usize)>)> {
+    let (s, container) = parse_color(s)?;
+    let (s, _) = tag(" bags contain ")(s)?;
+    let (s, containees) = terminated(
+        alt((
+            separated_list1(tag(", "), parse_quantified_bag),
+            value(Vec::new(), tag("no other bags")),
+        )),
+        char('.'),
+    )(s)?;
+    Ok((s, (container, containees)))
+}
+fn parse_input(s: &str) -> IResult<&str, Vec<(&str, Vec<(&str, usize)>)>> {
+    all_consuming(separated_list1(line_ending, parse_line))(s)
+}
+
+trait Solution {
+    fn part_1(&self) -> usize;
+}
+impl Solution for str {
+    fn part_1(&self) -> usize {
+        bfs(
+            "shiny gold",
+            &build_containee_map(&parse_input(self).expect("Failed to parse the input").1),
+        )
+        .len()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_input() {
+        assert_eq!(
+            parse_input(
+                "\
+light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags."
+            ),
+            Ok((
+                "",
+                vec![
+                    ("light red", vec![("bright white", 1), ("muted yellow", 2)]),
+                    (
+                        "dark orange",
+                        vec![("bright white", 3), ("muted yellow", 4)]
+                    ),
+                    ("bright white", vec![("shiny gold", 1)]),
+                    ("muted yellow", vec![("shiny gold", 2), ("faded blue", 9)]),
+                    ("shiny gold", vec![("dark olive", 1), ("vibrant plum", 2)]),
+                    ("dark olive", vec![("faded blue", 3), ("dotted black", 4)]),
+                    ("vibrant plum", vec![("faded blue", 5), ("dotted black", 6)]),
+                    ("faded blue", vec![]),
+                    ("dotted black", vec![]),
+                ]
+            ))
+        );
+    }
+
+    #[test]
+    fn example_1() {
+        assert_eq!(
+            bfs(
+                "shiny gold",
+                &build_containee_map(&[
+                    ("light red", vec![("bright white", 1), ("muted yellow", 2)]),
+                    (
+                        "dark orange",
+                        vec![("bright white", 3), ("muted yellow", 4)]
+                    ),
+                    ("bright white", vec![("shiny gold", 1)]),
+                    ("muted yellow", vec![("shiny gold", 2), ("faded blue", 9)]),
+                    ("shiny gold", vec![("dark olive", 1), ("vibrant plum", 2)]),
+                    ("dark olive", vec![("faded blue", 3), ("dotted black", 4)]),
+                    ("vibrant plum", vec![("faded blue", 5), ("dotted black", 6)]),
+                    ("faded blue", vec![]),
+                    ("dotted black", vec![]),
+                ])
+            ),
+            ["bright white", "muted yellow", "dark orange", "light red"]
+                .iter()
+                .collect()
+        );
+    }
+
+    #[test]
+    fn part_1() {
+        assert_eq!(include_str!("inputs/day_7").part_1(), 142);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod binary_boarding;
 pub mod custom_customs;
+pub mod handy_haversacks;
 pub mod passport_processing;
 pub mod password_philosophy;
 pub mod report_repair;
