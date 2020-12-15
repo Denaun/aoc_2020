790c1ee70f72178be60bb2899a5d061530a0b59e
Denaun <mauzuc90@yahoo.it>
2020-12-15 22:05:55 +0100

    Implement day 15 (part 2).

--- src/rambunctious_recitation.rs
+++ src/rambunctious_recitation.rs
@@ -30,11 +30,17 @@ fn parse_input<T: FromStr>(text: &str) -> Result<Vec<T>, <T as FromStr>::Err> {
 
 trait Solution {
     fn part_1(&self) -> usize;
+    fn part_2(&self) -> usize;
 }
 impl Solution for str {
     fn part_1(&self) -> usize {
         speak_numbers(parse_input(self).expect("Failed to parse the input"))
             .nth(2019)
+            .expect("Empty input")
+    }
+    fn part_2(&self) -> usize {
+        speak_numbers(parse_input(self).expect("Failed to parse the input"))
+            .nth(30_000_000 - 1)
             .expect("Empty input")
     }
 }
@@ -86,5 +92,57 @@ mod tests {
     #[test]
     fn part_1() {
         assert_eq!(include_str!("inputs/day_15").part_1(), 421);
+    }
+
+    #[test]
+    fn example_7() {
+        assert_eq!(
+            speak_numbers(vec![0, 3, 6]).nth(30_000_000 - 1),
+            Some(175594)
+        );
+    }
+
+    #[test]
+    fn example_8() {
+        assert_eq!(speak_numbers(vec![1, 3, 2]).nth(30_000_000 - 1), Some(2578));
+    }
+
+    #[test]
+    fn example_9() {
+        assert_eq!(
+            speak_numbers(vec![2, 1, 3]).nth(30_000_000 - 1),
+            Some(3544142)
+        );
+    }
+
+    #[test]
+    fn example_10() {
+        assert_eq!(
+            speak_numbers(vec![1, 2, 3]).nth(30_000_000 - 1),
+            Some(261214)
+        );
+    }
+
+    #[test]
+    fn example_11() {
+        assert_eq!(
+            speak_numbers(vec![2, 3, 1]).nth(30_000_000 - 1),
+            Some(6895259)
+        );
+    }
+
+    #[test]
+    fn example_12() {
+        assert_eq!(speak_numbers(vec![3, 2, 1]).nth(30_000_000 - 1), Some(18));
+    }
+
+    #[test]
+    fn example_13() {
+        assert_eq!(speak_numbers(vec![3, 1, 2]).nth(30_000_000 - 1), Some(362));
+    }
+
+    #[test]
+    fn part_2() {
+        assert_eq!(include_str!("inputs/day_15").part_2(), 436);
     }
 }
