cdfbf18fdc0c83dd404c1b2e395a659ecf6f9903
Denaun <mauzuc90@yahoo.it>
2020-12-22 00:11:34 +0100

    Implement day 22 (part 2).

--- src/allergen_assessment.rs
+++ src/allergen_assessment.rs
@@ -1,5 +1,6 @@
 //! Day 21
 
+use itertools::Itertools;
 use nom::{
     bytes::streaming::tag,
     character::complete::{alpha1, char, line_ending},
@@ -7,9 +8,11 @@ use nom::{
     sequence::{delimited, pair},
     IResult,
 };
-use std::collections::{HashMap, HashSet};
+use std::collections::{BTreeMap, HashMap, HashSet};
 
-fn find_non_allergens<'a>(notes: &[(Vec<&'a str>, Vec<&str>)]) -> Vec<&'a str> {
+fn get_allergen_candidates<'a, 'b>(
+    notes: &[(Vec<&'a str>, Vec<&'b str>)],
+) -> HashMap<&'b str, HashSet<&'a str>> {
     let mut allergen_candidates = HashMap::<&str, HashSet<&str>>::new();
     for (ingredients, allergens) in notes {
         for allergen in allergens {
@@ -21,6 +24,29 @@ fn find_non_allergens<'a>(notes: &[(Vec<&'a str>, Vec<&str>)]) -> Vec<&'a str> {
                 .or_insert(ingredients.iter().copied().collect::<HashSet<_>>());
         }
     }
+    allergen_candidates
+}
+fn find_allergens<'a, 'b>(
+    notes: &[(Vec<&'a str>, Vec<&'b str>)],
+) -> Option<BTreeMap<&'b str, &'a str>> {
+    let mut allergen_candidates = get_allergen_candidates(notes);
+    let mut allergens = BTreeMap::new();
+    // Same idea as Ticket Translation.
+    while let Some((&allergen, candidate)) = allergen_candidates.iter().find(|c| c.1.len() == 1) {
+        let candidate = *candidate.iter().exactly_one().unwrap();
+        for (_, candidates) in allergen_candidates.iter_mut() {
+            candidates.remove(&candidate);
+        }
+        allergens.insert(allergen, candidate);
+    }
+    if allergen_candidates.iter().any(|(_, c)| !c.is_empty()) {
+        None
+    } else {
+        Some(allergens)
+    }
+}
+fn find_non_allergens<'a>(notes: &[(Vec<&'a str>, Vec<&str>)]) -> Vec<&'a str> {
+    let allergen_candidates = get_allergen_candidates(notes);
     notes
         .iter()
         .flat_map(|(ingredients, _)| {
@@ -49,17 +75,25 @@ fn parse_input(s: &str) -> IResult<&str, Vec<(Vec<&str>, Vec<&str>)>> {
 
 trait Solution {
     fn part_1(&self) -> usize;
+    fn part_2(&self) -> String;
 }
 impl Solution for &str {
     fn part_1(&self) -> usize {
         find_non_allergens(&parse_input(self).expect("Failed to parse the input").1).len()
+    }
+    fn part_2(&self) -> String {
+        find_allergens(&parse_input(self).expect("Failed to parse the input").1)
+            .expect("Allergens not found")
+            .iter()
+            .map(|(_, ingredient)| ingredient)
+            .join(",")
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use itertools::{assert_equal, Itertools};
+    use itertools::assert_equal;
 
     #[test]
     fn example_1() {
@@ -84,5 +118,35 @@ sqjhc mxmxvkd sbzzf (contains fish)",
     #[test]
     fn part_1() {
         assert_eq!(include_str!("inputs/day_21").part_1(), 1885)
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(
+            find_allergens(
+                &parse_input(
+                    "\
+mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+trh fvjkl sbzzf mxmxvkd (contains dairy)
+sqjhc fvjkl (contains soy)
+sqjhc mxmxvkd sbzzf (contains fish)",
+                )
+                .unwrap()
+                .1,
+            )
+            .unwrap()
+            .iter()
+            .map(|(_, ingredient)| ingredient)
+            .join(","),
+            "mxmxvkd,sqjhc,fvjkl"
+        )
+    }
+
+    #[test]
+    fn part_2() {
+        assert_eq!(
+            include_str!("inputs/day_21").part_2(),
+            "fllssz,kgbzf,zcdcdf,pzmg,kpsdtv,fvvrc,dqbjj,qpxhfp"
+        )
     }
 }
