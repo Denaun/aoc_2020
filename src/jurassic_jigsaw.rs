bc39eefc12740f933542aaa6f107f0a31b058822
Maurizio Zucchelli <mauzuc90@yahoo.it>
2020-12-25 18:08:14 +0100

    Rewrite day 20 (part 1).
    Main points from https://github.com/tginsberg/advent-2020-kotlin.

--- src/jurassic_jigsaw.rs
+++ src/jurassic_jigsaw.rs
@@ -1,75 +1,109 @@
 //! Day 20
 
 use crate::docking_data::parse_integer;
-use itertools::Itertools;
+use bitvec::prelude::*;
+use itertools::{iterate, Itertools};
 use nom::{
-    bytes::complete::{is_a, tag},
+    branch::alt,
+    bytes::complete::tag,
     character::complete::{char, line_ending},
-    combinator::map,
-    multi::separated_list1,
+    combinator::{map, value},
+    multi::{many1, separated_list1},
     sequence::{delimited, pair, separated_pair},
     IResult,
 };
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-struct TileId(usize);
-type Tile<'a> = Vec<&'a str>;
-
-fn find_corners(tiles: &[(TileId, Tile)]) -> Vec<TileId> {
-    let mut borders = HashMap::<_, Vec<_>>::new();
-    for (id, tile) in tiles {
-        let top = tile.first().unwrap().to_string();
-        let bottom = tile.last().unwrap().to_string();
-        let left = tile.iter().map(|row| row.chars().next().unwrap()).join("");
-        let right = tile.iter().map(|row| row.chars().last().unwrap()).join("");
-        borders.entry(reversed(&top)).or_default().push(*id);
-        borders.entry(reversed(&bottom)).or_default().push(*id);
-        borders.entry(reversed(&left)).or_default().push(*id);
-        borders.entry(reversed(&right)).or_default().push(*id);
-        borders.entry(top).or_default().push(*id);
-        borders.entry(bottom).or_default().push(*id);
-        borders.entry(left).or_default().push(*id);
-        borders.entry(right).or_default().push(*id);
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Tile {
+    id: usize,
+    data: Vec<BitVec>,
+    sides: HashSet<BitVec>,
+    sides_reversed: HashSet<BitVec>,
+}
+impl Tile {
+    pub fn new(id: usize, data: Vec<BitVec>) -> Self {
+        let sides = Orientation::iter()
+            .map(|o| Self::side_facing(&data, o))
+            .collect::<HashSet<_>>();
+        let sides_reversed = sides
+            .iter()
+            .map(|side| side.iter().rev().copied().collect())
+            .collect();
+        Self {
+            id,
+            data,
+            sides,
+            sides_reversed,
+        }
     }
-    let edges = borders
-        .into_iter()
-        .filter_map(|(_, tiles)| {
-            if tiles.len() == 1 {
-                Some(tiles[0])
-            } else {
-                None
-            }
-        })
-        .collect_vec();
-    edges
-        .iter()
-        .unique()
-        .filter_map(|id| {
-            if edges.iter().filter(|&i| i == id).count() == 4 {
-                Some(*id)
-            } else {
-                None
-            }
-        })
-        .collect_vec()
-}
-fn reversed(s: &str) -> String {
-    s.chars().rev().collect()
+    pub fn shared_side_count(&self, tiles: &[Tile]) -> usize {
+        self.sides
+            .iter()
+            .map(|side| {
+                tiles
+                    .iter()
+                    .filter(|tile| tile.id != self.id && tile.has_side(side))
+                    .count()
+            })
+            .sum()
+    }
+    fn has_side(&self, side: &BitSlice) -> bool {
+        self.sides.contains(side) || self.sides_reversed.contains(side)
+    }
+
+    fn side_facing(data: &[BitVec], orientation: Orientation) -> BitVec {
+        match orientation {
+            Orientation::Top => data.first().unwrap().iter().collect(),
+            Orientation::Bottom => data.last().unwrap().iter().collect(),
+            Orientation::Left => data.iter().map(|row| row.first().unwrap()).collect(),
+            Orientation::Right => data.iter().map(|row| row.last().unwrap()).collect(),
+        }
+    }
 }
 
-fn parse_input(s: &str) -> IResult<&str, Vec<(TileId, Tile)>> {
+#[derive(Debug, Clone, Copy)]
+enum Orientation {
+    Top,
+    Bottom,
+    Left,
+    Right,
+}
+impl Orientation {
+    fn iter() -> impl Iterator<Item = Self> + Clone {
+        [Self::Top, Self::Bottom, Self::Left, Self::Right]
+            .iter()
+            .copied()
+    }
+}
+
+fn corners<'a>(tiles: &'a [Tile]) -> impl Iterator<Item = &'a Tile> {
+    tiles
+        .iter()
+        .filter(move |tile| tile.shared_side_count(tiles) == 2)
+}
+
+fn parse_input(s: &str) -> IResult<&str, Vec<Tile>> {
     separated_list1(
         pair(line_ending, line_ending),
-        separated_pair(
-            delimited(tag("Tile "), map(parse_integer, |id| TileId(id)), char(':')),
-            line_ending,
-            parse_tile,
+        map(
+            separated_pair(
+                delimited(tag("Tile "), parse_integer, char(':')),
+                line_ending,
+                parse_tile_data,
+            ),
+            |(id, data)| Tile::new(id, data),
         ),
     )(s)
 }
-fn parse_tile(s: &str) -> IResult<&str, Tile> {
-    separated_list1(line_ending, is_a("#."))(s)
+fn parse_tile_data(s: &str) -> IResult<&str, Vec<BitVec>> {
+    separated_list1(
+        line_ending,
+        map(
+            many1(alt((value(true, char('#')), value(false, char('.'))))),
+            |bits| bits.into_iter().collect(),
+        ),
+    )(s)
 }
 
 trait Solution {
@@ -78,7 +112,7 @@ trait Solution {
 impl Solution for str {
     fn part_1(&self) -> usize {
         let tiles = parse_input(self).expect("Failed to parse the input").1;
-        find_corners(&tiles).iter().map(|id| id.0).product()
+        corners(&tiles).map(|tile| tile.id).product()
     }
 }
 
@@ -203,139 +237,139 @@ Tile 3079:
             Ok((
                 "",
                 [
-                    (
-                        TileId(2311),
+                    Tile::new(
+                        2311,
                         vec![
-                            "..##.#..#.",
-                            "##..#.....",
-                            "#...##..#.",
-                            "####.#...#",
-                            "##.##.###.",
-                            "##...#.###",
-                            ".#.#.#..##",
-                            "..#....#..",
-                            "###...#.#.",
-                            "..###..###",
+                            bitvec![0, 0, 1, 1, 0, 1, 0, 0, 1, 0,],
+                            bitvec![1, 1, 0, 0, 1, 0, 0, 0, 0, 0,],
+                            bitvec![1, 0, 0, 0, 1, 1, 0, 0, 1, 0,],
+                            bitvec![1, 1, 1, 1, 0, 1, 0, 0, 0, 1,],
+                            bitvec![1, 1, 0, 1, 1, 0, 1, 1, 1, 0,],
+                            bitvec![1, 1, 0, 0, 0, 1, 0, 1, 1, 1,],
+                            bitvec![0, 1, 0, 1, 0, 1, 0, 0, 1, 1,],
+                            bitvec![0, 0, 1, 0, 0, 0, 0, 1, 0, 0,],
+                            bitvec![1, 1, 1, 0, 0, 0, 1, 0, 1, 0,],
+                            bitvec![0, 0, 1, 1, 1, 0, 0, 1, 1, 1,],
                         ]
                     ),
-                    (
-                        TileId(1951),
+                    Tile::new(
+                        1951,
                         vec![
-                            "#.##...##.",
-                            "#.####...#",
-                            ".....#..##",
-                            "#...######",
-                            ".##.#....#",
-                            ".###.#####",
-                            "###.##.##.",
-                            ".###....#.",
-                            "..#.#..#.#",
-                            "#...##.#..",
+                            bitvec![1, 0, 1, 1, 0, 0, 0, 1, 1, 0,],
+                            bitvec![1, 0, 1, 1, 1, 1, 0, 0, 0, 1,],
+                            bitvec![0, 0, 0, 0, 0, 1, 0, 0, 1, 1,],
+                            bitvec![1, 0, 0, 0, 1, 1, 1, 1, 1, 1,],
+                            bitvec![0, 1, 1, 0, 1, 0, 0, 0, 0, 1,],
+                            bitvec![0, 1, 1, 1, 0, 1, 1, 1, 1, 1,],
+                            bitvec![1, 1, 1, 0, 1, 1, 0, 1, 1, 0,],
+                            bitvec![0, 1, 1, 1, 0, 0, 0, 0, 1, 0,],
+                            bitvec![0, 0, 1, 0, 1, 0, 0, 1, 0, 1,],
+                            bitvec![1, 0, 0, 0, 1, 1, 0, 1, 0, 0,],
                         ]
                     ),
-                    (
-                        TileId(1171),
+                    Tile::new(
+                        1171,
                         vec![
-                            "####...##.",
-                            "#..##.#..#",
-                            "##.#..#.#.",
-                            ".###.####.",
-                            "..###.####",
-                            ".##....##.",
-                            ".#...####.",
-                            "#.##.####.",
-                            "####..#...",
-                            ".....##...",
+                            bitvec![1, 1, 1, 1, 0, 0, 0, 1, 1, 0,],
+                            bitvec![1, 0, 0, 1, 1, 0, 1, 0, 0, 1,],
+                            bitvec![1, 1, 0, 1, 0, 0, 1, 0, 1, 0,],
+                            bitvec![0, 1, 1, 1, 0, 1, 1, 1, 1, 0,],
+                            bitvec![0, 0, 1, 1, 1, 0, 1, 1, 1, 1,],
+                            bitvec![0, 1, 1, 0, 0, 0, 0, 1, 1, 0,],
+                            bitvec![0, 1, 0, 0, 0, 1, 1, 1, 1, 0,],
+                            bitvec![1, 0, 1, 1, 0, 1, 1, 1, 1, 0,],
+                            bitvec![1, 1, 1, 1, 0, 0, 1, 0, 0, 0,],
+                            bitvec![0, 0, 0, 0, 0, 1, 1, 0, 0, 0,],
                         ]
                     ),
-                    (
-                        TileId(1427),
+                    Tile::new(
+                        1427,
                         vec![
-                            "###.##.#..",
-                            ".#..#.##..",
-                            ".#.##.#..#",
-                            "#.#.#.##.#",
-                            "....#...##",
-                            "...##..##.",
-                            "...#.#####",
-                            ".#.####.#.",
-                            "..#..###.#",
-                            "..##.#..#.",
+                            bitvec![1, 1, 1, 0, 1, 1, 0, 1, 0, 0,],
+                            bitvec![0, 1, 0, 0, 1, 0, 1, 1, 0, 0,],
+                            bitvec![0, 1, 0, 1, 1, 0, 1, 0, 0, 1,],
+                            bitvec![1, 0, 1, 0, 1, 0, 1, 1, 0, 1,],
+                            bitvec![0, 0, 0, 0, 1, 0, 0, 0, 1, 1,],
+                            bitvec![0, 0, 0, 1, 1, 0, 0, 1, 1, 0,],
+                            bitvec![0, 0, 0, 1, 0, 1, 1, 1, 1, 1,],
+                            bitvec![0, 1, 0, 1, 1, 1, 1, 0, 1, 0,],
+                            bitvec![0, 0, 1, 0, 0, 1, 1, 1, 0, 1,],
+                            bitvec![0, 0, 1, 1, 0, 1, 0, 0, 1, 0,],
                         ]
                     ),
-                    (
-                        TileId(1489),
+                    Tile::new(
+                        1489,
                         vec![
-                            "##.#.#....",
-                            "..##...#..",
-                            ".##..##...",
-                            "..#...#...",
-                            "#####...#.",
-                            "#..#.#.#.#",
-                            "...#.#.#..",
-                            "##.#...##.",
-                            "..##.##.##",
-                            "###.##.#..",
+                            bitvec![1, 1, 0, 1, 0, 1, 0, 0, 0, 0,],
+                            bitvec![0, 0, 1, 1, 0, 0, 0, 1, 0, 0,],
+                            bitvec![0, 1, 1, 0, 0, 1, 1, 0, 0, 0,],
+                            bitvec![0, 0, 1, 0, 0, 0, 1, 0, 0, 0,],
+                            bitvec![1, 1, 1, 1, 1, 0, 0, 0, 1, 0,],
+                            bitvec![1, 0, 0, 1, 0, 1, 0, 1, 0, 1,],
+                            bitvec![0, 0, 0, 1, 0, 1, 0, 1, 0, 0,],
+                            bitvec![1, 1, 0, 1, 0, 0, 0, 1, 1, 0,],
+                            bitvec![0, 0, 1, 1, 0, 1, 1, 0, 1, 1,],
+                            bitvec![1, 1, 1, 0, 1, 1, 0, 1, 0, 0,],
                         ]
                     ),
-                    (
-                        TileId(2473),
+                    Tile::new(
+                        2473,
                         vec![
-                            "#....####.",
-                            "#..#.##...",
-                            "#.##..#...",
-                            "######.#.#",
-                            ".#...#.#.#",
-                            ".#########",
-                            ".###.#..#.",
-                            "########.#",
-                            "##...##.#.",
-                            "..###.#.#.",
+                            bitvec![1, 0, 0, 0, 0, 1, 1, 1, 1, 0,],
+                            bitvec![1, 0, 0, 1, 0, 1, 1, 0, 0, 0,],
+                            bitvec![1, 0, 1, 1, 0, 0, 1, 0, 0, 0,],
+                            bitvec![1, 1, 1, 1, 1, 1, 0, 1, 0, 1,],
+                            bitvec![0, 1, 0, 0, 0, 1, 0, 1, 0, 1,],
+                            bitvec![0, 1, 1, 1, 1, 1, 1, 1, 1, 1,],
+                            bitvec![0, 1, 1, 1, 0, 1, 0, 0, 1, 0,],
+                            bitvec![1, 1, 1, 1, 1, 1, 1, 1, 0, 1,],
+                            bitvec![1, 1, 0, 0, 0, 1, 1, 0, 1, 0,],
+                            bitvec![0, 0, 1, 1, 1, 0, 1, 0, 1, 0,],
                         ]
                     ),
-                    (
-                        TileId(2971),
+                    Tile::new(
+                        2971,
                         vec![
-                            "..#.#....#",
-                            "#...###...",
-                            "#.#.###...",
-                            "##.##..#..",
-                            ".#####..##",
-                            ".#..####.#",
-                            "#..#.#..#.",
-                            "..####.###",
-                            "..#.#.###.",
-                            "...#.#.#.#",
+                            bitvec![0, 0, 1, 0, 1, 0, 0, 0, 0, 1,],
+                            bitvec![1, 0, 0, 0, 1, 1, 1, 0, 0, 0,],
+                            bitvec![1, 0, 1, 0, 1, 1, 1, 0, 0, 0,],
+                            bitvec![1, 1, 0, 1, 1, 0, 0, 1, 0, 0,],
+                            bitvec![0, 1, 1, 1, 1, 1, 0, 0, 1, 1,],
+                            bitvec![0, 1, 0, 0, 1, 1, 1, 1, 0, 1,],
+                            bitvec![1, 0, 0, 1, 0, 1, 0, 0, 1, 0,],
+                            bitvec![0, 0, 1, 1, 1, 1, 0, 1, 1, 1,],
+                            bitvec![0, 0, 1, 0, 1, 0, 1, 1, 1, 0,],
+                            bitvec![0, 0, 0, 1, 0, 1, 0, 1, 0, 1,],
                         ]
                     ),
-                    (
-                        TileId(2729),
+                    Tile::new(
+                        2729,
                         vec![
-                            "...#.#.#.#",
-                            "####.#....",
-                            "..#.#.....",
-                            "....#..#.#",
-                            ".##..##.#.",
-                            ".#.####...",
-                            "####.#.#..",
-                            "##.####...",
-                            "##..#.##..",
-                            "#.##...##.",
+                            bitvec![0, 0, 0, 1, 0, 1, 0, 1, 0, 1,],
+                            bitvec![1, 1, 1, 1, 0, 1, 0, 0, 0, 0,],
+                            bitvec![0, 0, 1, 0, 1, 0, 0, 0, 0, 0,],
+                            bitvec![0, 0, 0, 0, 1, 0, 0, 1, 0, 1,],
+                            bitvec![0, 1, 1, 0, 0, 1, 1, 0, 1, 0,],
+                            bitvec![0, 1, 0, 1, 1, 1, 1, 0, 0, 0,],
+                            bitvec![1, 1, 1, 1, 0, 1, 0, 1, 0, 0,],
+                            bitvec![1, 1, 0, 1, 1, 1, 1, 0, 0, 0,],
+                            bitvec![1, 1, 0, 0, 1, 0, 1, 1, 0, 0,],
+                            bitvec![1, 0, 1, 1, 0, 0, 0, 1, 1, 0,],
                         ]
                     ),
-                    (
-                        TileId(3079),
+                    Tile::new(
+                        3079,
                         vec![
-                            "#.#.#####.",
-                            ".#..######",
-                            "..#.......",
-                            "######....",
-                            "####.#..#.",
-                            ".#...#.##.",
-                            "#.#####.##",
-                            "..#.###...",
-                            "..#.......",
-                            "..#.###...",
+                            bitvec![1, 0, 1, 0, 1, 1, 1, 1, 1, 0,],
+                            bitvec![0, 1, 0, 0, 1, 1, 1, 1, 1, 1,],
+                            bitvec![0, 0, 1, 0, 0, 0, 0, 0, 0, 0,],
+                            bitvec![1, 1, 1, 1, 1, 1, 0, 0, 0, 0,],
+                            bitvec![1, 1, 1, 1, 0, 1, 0, 0, 1, 0,],
+                            bitvec![0, 1, 0, 0, 0, 1, 0, 1, 1, 0,],
+                            bitvec![1, 0, 1, 1, 1, 1, 1, 0, 1, 1,],
+                            bitvec![0, 0, 1, 0, 1, 1, 1, 0, 0, 0,],
+                            bitvec![0, 0, 1, 0, 0, 0, 0, 0, 0, 0,],
+                            bitvec![0, 0, 1, 0, 1, 1, 1, 0, 0, 0,],
                         ]
                     )
                 ]
@@ -349,10 +383,10 @@ Tile 3079:
     #[test]
     fn example_1() {
         assert_equal(
-            find_corners(&parse_input(EXAMPLE_INPUT).unwrap().1)
-                .iter()
+            corners(&parse_input(EXAMPLE_INPUT).unwrap().1)
+                .map(|tile| &tile.id)
                 .sorted(),
-            &[TileId(1171), TileId(1951), TileId(2971), TileId(3079)],
+            &[1171, 1951, 2971, 3079],
         );
     }
 
